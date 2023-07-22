9ab8888f58005042ca5f03ffa18fc1c6b9432cca
robertocannella <robertocannella@gmail.com>
2023-07-22 11:59:35 -0400

    Initial commit

---
+++ rc-slider.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+Plugin Name: Rc Slider
+Plugin URI: http://robertocannella.com/wordpress
+Description: A slider plugin
+Version: 1.0
+Author: Roberto Cannella
+Author URI: http://robertocannella.com_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+Requires: 6.2
+Text Domain: rc-slider
+Domain Path: /languages
+*/
+
+/*
+{Rc Slider} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+{Rc Slider} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with {Rc Slider}. If not, see {ttp://robertocannella.com/wordpress}.
+*/
+
+if (!defined('ABS_PATH')) {die("-not allowed"); exit;}
+
+if (! class_exists( 'RC_Slider ' ) ) {
+    class RC_Slider {
+
+    }
+
+}
